d4acd7c6a0af1d14b5d0bcbae93d363b541c695d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-12-01 13:46:01 -0800

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#! usr/bin/python3
+
+if __name__ == "__main__":
+
+    founder = ("Quincy","Lucy","Rob")
+
+count = 0
+
+for elements in founder:
+
+        count += 1
+
+print('{}: {}' .format(count,elements,)))
