fc3cb3e0fcc0d8c0db14ee6651c54f548fe02782
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-12-02 07:47:34 -0800

    calculate

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,4 @@
-#! usr/bin/python3
+#!/usr/bin/python3
 
 if __name == "__main__":
 
@@ -16,4 +16,4 @@ print('{:d} - {:d} = {:d}' .format(a, b, (sub(a, b)))
 
 print('{:d} * {:d} = {:d}' .format(a, b, (mul(a, b)))
 
-print('{:d} / {:d} = {:d}'.format(a, b,  (div(a,b)))h: 1: usr/bin/python3: not found
+print('{:d} / {:d} = {:d}'.format(a, b,  (div(a,b)))
