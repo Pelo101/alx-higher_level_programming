81b51a4eb627e9e89cb13f2e5fa488cd8ebb51be
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-12-05 12:41:19 -0800

    element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0 > len(my_list):
+        return None
+    else:
+        'return, my_list[idx]'
+    print('{:d}'.format(my_list[idx]))
