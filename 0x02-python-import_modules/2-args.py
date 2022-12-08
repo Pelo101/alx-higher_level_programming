453aebd6c7c84ecc83dbc4bfd65a3c4c573097fd
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-12-07 21:17:13 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+    count = len(sys.argv)
+    print("{} arguments:".format(count))
+for elements in sys.argv:
+    count += 1
+print('{}:{}'.format(sys.argv.index(elements), elements))
