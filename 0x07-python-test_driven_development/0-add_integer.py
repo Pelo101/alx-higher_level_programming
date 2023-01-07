eef88ac7a22f59f353f683282004f701d5748343
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-01-07 05:18:50 -0800

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -24,11 +24,10 @@ def add(a, b=98):
 
     """
 
-
-    if a is not isinstance(a, int) and not isinstance(a, float):
-         raise TypeError("a must be an integer")
-    if b is not isinstance(b, int) and not isinstance(b, float):
-         raise TypeError("b must be an integer")
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
 
     a = int(a)
     b = int(b)
