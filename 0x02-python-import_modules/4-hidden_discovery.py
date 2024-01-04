966b79b83032439be68f69fe0faa05104d30849f
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-04 08:08:55 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4 
+    import hidden_4
     name = dir(hidden_4)
     for i in range(len(name)):
         if name[i][0] != "_" and name[i][1] != "_":
