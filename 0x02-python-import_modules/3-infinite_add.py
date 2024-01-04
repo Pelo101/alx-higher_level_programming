9b957f314d69f3bbf2cacbb003db9f5edea2a193
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-04 07:49:51 -0800

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    sum = 0
+    total_sum = 0
     for i in range(1, len(sys.argv)):
-        sum = sum + int(sys.argv[i])
-        print(sum)
+        total_sum +=  int(sys.argv[i])
+        print(total_sum)
