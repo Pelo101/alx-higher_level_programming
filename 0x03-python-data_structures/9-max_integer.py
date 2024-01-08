aaceeac1c184c7a076de7d67a2bded223149bea5
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-08 09:15:23 -0800

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        my_list.sort()
-        return((my_list[-1]))
+        sorted_list = sorted(my_list)
+        return (sorted_list[-1])
