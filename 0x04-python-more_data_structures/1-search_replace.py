b655221676a7e6a14244cd66442f3729ce83ee3d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-10 05:06:03 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
 
     return[replace if item == search else item for item in my_list]
