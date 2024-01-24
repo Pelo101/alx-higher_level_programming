d0d09a95ab867c16169352f3285fc708a0077cbc
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-23 23:04:27 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A module that defines a sqaure """
+
+
+class Square:
+    """ A class that defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """ A method to define size and retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ A method that returns a current square"""
+        return self.__size ** 2
