94170bdcd5a1daea9749ff107b90e442bcd9b3e4
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-23 20:43:11 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ A class that defines a square (based on 1-square.py) """
+
+
 class Square:
     def __init__(self, size=0):
-        
+
         self.__size = 0
 
         if type(size) != int:
