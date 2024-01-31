da2f415f3f92739fbc52a5c436d1bc63f04d967d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-31 04:30:03 -0800

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -18,9 +18,9 @@ class Rectangle:
     def width(self, value):
         """Property setter to set the width"""
         if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -32,17 +32,17 @@ class Rectangle:
     def height(self, value):
         """Property setter to set the height"""
         if not isinstance(value, int):
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError('height must >= 0')
+            raise ValueError("height must >= 0")
         self.__height = value
 
     def area(self):
         """Return the area of the rectangle"""
-        return(self.__width * self.__height)
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Returns the rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return((self.__height * 2) + (self.__width * 2))
+        return ((self.__height * 2) + (self.__width * 2))
