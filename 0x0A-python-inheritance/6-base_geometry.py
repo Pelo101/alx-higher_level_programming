765ba4c379d08dd237bfc933d49e99174396944d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-05 09:31:43 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A module that  is doing base geometry"""
+
+
+class BaseGeometry:
+    """ A class """
+
+    def area(self):
+        """ An instance method"""
+        raise Exception("area() is not implemented")
