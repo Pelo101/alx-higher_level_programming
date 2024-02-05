a306e0858b480b867310768115cde93cc5c5b2c0
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-05 02:48:18 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Module that refers to a function that lists attributes
+    of classes and instances"""
+
 
 def lookup(obj):
     """ Function that returns the list of available attributes"""
