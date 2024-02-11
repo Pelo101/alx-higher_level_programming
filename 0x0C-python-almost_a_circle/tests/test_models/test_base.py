34ab30bf90b462ad40cd91b5e7d38b9cea721e5c
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-11 01:00:59 -0800

    test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,33 +3,37 @@
 import unittest
 from models.base import Base
 
-class test_Baseclass(Unittest.TestCase):
+
+class test_Baseclass(unittest.TestCase):
     """ Test base class"""
 
-    if __name__ == '__main__':
-
-        def test_base_id(self):
+    def test_base_id(self):
 
              obj1 = Base()
-             self.assertEqual(obj1.id == 1)
+             self.assertEqual(obj1.id, 1)
 
              obj2 = Base()
-             self.assertEqual(obj2.id == 2)
+             self.assertEqual(obj2.id, 2)
 
              obj3 = Base()
-             self.assertEqual(obj3.id == 3)
+             self.assertEqual(obj3.id, 3)
 
              obj4 = Base(12)
-             self.assertEqual(obj4.id == 12)
+             self.assertEqual(obj4.id, 12)
 
              obj5 = Base(4)
-             self.assertEqual(obj5.id == 5)
+             self.assertEqual(obj5.id, 4)
 
              obj6 = Base(5.5)
-             self.assertEqual(obj6.id == 5.5)
+             self.assertEqual(obj6.id, 5.5)
 
              obj7 = Base(-4)
-             self.assertEqual(obj7.id == -4)
+             self.assertEqual(obj7.id, -4)
 
              obj8 = Base(None)
-             self.assertEqual(obj8.id == None)
+             self.assertEqual(obj8.id, 4)
+
+
+    if __name__ == '__main__':
+        unittest.main()
+
