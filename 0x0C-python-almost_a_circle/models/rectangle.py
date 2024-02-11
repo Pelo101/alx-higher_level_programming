81d96f1478e323aead86df831ec8cded271bc948
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-11 09:38:20 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,12 @@ class Rectangle(Base):
 
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return f"[Rectangle]({id(self)}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+
+
+
+
+
