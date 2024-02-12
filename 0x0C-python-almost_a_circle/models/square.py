5172220c6e19030e35a2e3d58dd62e638c728c16
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-12 10:44:08 -0800

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that defines a sqaure"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A class that creates a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Returns the string representation of square"""
+
+        return '[{}] ({}) {}/{} - {}' .\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
