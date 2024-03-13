f2eb55fdb0b236750885369e5f6675af8e68264c
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-03-13 03:25:28 -0700

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE IF EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
-INSERT INTO second_table(id, name, score)VALUES(5, "John", 10);
-INSERT INTO second_table(id, name, score)VALUES(5, "Alex", 3);
-INSERT INTO second_table(id, name, score)VALUES(5, "Bob", 14);
-INSERT INTO second_table(id, name, score)VALUES(5, "George", 8);
+INSERT INTO second_table('id','name','score')VALUES(5, "John", 10);
+INSERT INTO second_table('id','name','score')VALUES(5, "Alex", 3);
+INSERT INTO second_table('id','name','score')VALUES(5, "Bob", 14);
+INSERT INTO second_table('id','name','score')VALUES(5, "George", 8);
 
