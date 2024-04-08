26d4c8c306a3b933a06d902c8d50b7ae5bacde97
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-04-08 03:51:46 -0700

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 //  script that prints the first argument passed to it
 
-const args = process.argv.slice(2);
+const argv = process.argv.slice(2);
 
-if (args[0] === undefined) {
-  console.log('No arguments');
-} else if (!args[1]);
-{
-  console.log(args[0]);
+if (argv[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argv[0]);
 }
