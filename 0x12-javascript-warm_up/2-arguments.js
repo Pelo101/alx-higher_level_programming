33d0f47991a5c4ec812f5c6db7c4a3a2c0af45e0
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-04-08 03:12:31 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that prints a message depending of the number of arguments passed
-if (process.argv.length === 2) {
-  console.log('No arguments');
-} else if (process.argv.length === 3) {
+if (process.argv.slice(2).length === 0) {
+  console.log('No argument');
+} else if (process.argv.slice(2).length === 1) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
