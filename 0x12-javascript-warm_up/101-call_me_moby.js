2d33eb3e93d42669a99aa6adfecae97f2b2fa89d
Pelo101clear <110992136+Pelo101@users.noreply.github.com>
2024-06-18 08:16:47 -0700

    task 15

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// function that executes x times
+
+function callMeMoby(x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports = {
+
+   callMeMoby: callMeMoby
+};
