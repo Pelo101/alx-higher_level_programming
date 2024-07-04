6f47d8c4ff51e68789852a7df0fb04847083c2ca
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-04 13:19:22 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -X  OPTIONS -I -L $1 | grep "Allow"
+curl -sI ALLOW $1 - | grep "ALLOW" | cut -d "" -f2-
