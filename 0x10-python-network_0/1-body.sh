3535dcb7ab4f2b6b217a460b4a699cd42c589206
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-04 12:31:33 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o - "$1" | grep -q  "Route 1" && echo "Route 2"
-
+curl -sX GET $1 -L
