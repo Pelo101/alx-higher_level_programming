00c85136ec620bdc7a366e30bdd55e19b29803d7
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-05 11:02:58 -0700

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email address, sends a POST request"""
+
+import requests
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    value = {"email": email}
+    response = request.post(url, data=value)
+    print(response.text)
