c36754e00907002e54c3993104a14f9851459433
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-05 05:17:14 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Python script that takes displays x-request-id"""
 import urllib.request
+import sys
 
-url = "https://alx-intranet.hbtn.io"
+if __name__ == "__main__":
+    url = sys.argv[1]
+
 with urllib.request.urlopen(url) as response:
     x_request_id = response.getheader("x-request-id")
     print(x_request_id)
