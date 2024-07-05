97714d47555752e0d2d788c06c3cd0ffd34d1789
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-05 11:22:33 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, handles errors"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = request.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
