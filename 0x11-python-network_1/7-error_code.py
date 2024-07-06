9a3468f46f47625feee569f7134c4d381199151a
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-07-06 04:57:15 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = request.get(url)
+    response = requests.get(url)
 
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
